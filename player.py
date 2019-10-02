0430c3940865445f37be271b20c30ce8ec323138
Roberto Camerite <roberto.silva@camerite.com>
2019-10-02 14:01:42 -0300

    modified playable cards rules

--- player.py
+++ player.py
@@ -172,13 +172,14 @@ class Player(object):
                 card.value == c.DRAW_TWO and self.game.draw_counter:
             self.logger.debug("Player has to draw and can't counter")
             is_playable = False
-        elif last.special == c.DRAW_FOUR and self.game.draw_counter:
+        elif last.special == c.DRAW_FOUR and not \
+                card.value == c.DRAW_TWO and self.game.draw_counter:
             self.logger.debug("Player has to draw and can't counter")
             is_playable = False
-        elif (last.special == c.CHOOSE or last.special == c.DRAW_FOUR) and \
-                (card.special == c.CHOOSE or card.special == c.DRAW_FOUR):
-            self.logger.debug("Can't play colorchooser on another one")
-            is_playable = False
+        # elif (last.special == c.CHOOSE or last.special == c.DRAW_FOUR) and \
+        #         (card.special == c.CHOOSE or card.special == c.DRAW_FOUR):
+        #     self.logger.debug("Can't play colorchooser on another one")
+        #     is_playable = False
         elif not last.color:
             self.logger.debug("Last card has no color")
             is_playable = False
